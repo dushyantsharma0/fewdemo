0570a871ad1f50b91a59c14785c201acc50b4d34
dushyantsharma0 <dushyantbhaman89@gmail.com>
2024-04-19 20:28:24 +0530

    add

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongoose= require('mongoose')
 mongoose.connect(process.env.MONGODB_URl)
 const express = require('express')
 const cors=require('cors')
-
+const modleSchma=require('./models/user')
+const PostSchema=require('./models/postes')
+const commentsSchema=require('./models/comments')
+const LikeSchema=require('./models/likes')
 
 const app = express()
 app.use(cors())
@@ -11,9 +14,34 @@ app.use(express.urlencoded({extended:false}))
 
 app.use(express.json())
 
-const routes =require('./routers/userRouter')
+app.get('/', function(req, res){
+    res.send('Hello World');
+   })
 
-app.use('/',routes)
+   app.get('/user', async function(req, res){
+    const data = await PostSchema.find().populate([
+      {
+        path: 'author',
+        select: 'name', // Selecting the 'name' field in the 'author' path
+      },
+      {
+        path: 'comment',
+        populate:{
+          path: 'sender',
+       
+        }
+      },   { path: "like",
+      populate:{
+       path: 'users'
+      }
+   }
+    ]);
+    res.send(data);
+  });
+  app.get('/user2', async function(req, res){
+    const data = await PostSchema.find()
+    res.send(data);
+  });
 
 
 app.listen(3001,()=>{
