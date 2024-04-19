46f25b2678c4bda193ab6805021c2e5eb35d1e9b
dushyantsharma0 <dushyantbhaman89@gmail.com>
2024-04-19 20:52:22 +0530

    add

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,30 @@
-const product=require("../models/products")
+const userSchma=require('../models/user');
+const PostSchma=require('../models/postes');
+const commentSchma=require('../models/comments');
+const likesSchma=require('../models/likes');
 
 const getAllProducts=async(req,resp)=>{
-    let products= await product.find()
-resp.status(200).json(products)
+
+
+
+   const data = await PostSchma.find().populate([
+    {
+      path: 'author',
+      select: 'name', // Selecting the 'name' field in the 'author' path
+    },
+    {
+      path: 'comment',
+      populate:{
+        path: 'sender',
+     
+      }
+    },   { path: "like",
+    populate:{
+     path: 'users'
+    }
+ }
+  ]);
+resp.status(200).json(data)
 };
 const getAllProductsTesting=async(req,resp)=>{
     resp.status(200).json({msg:"i am get all getAllProductsTesting"})
