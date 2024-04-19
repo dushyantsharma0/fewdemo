6d67185aa68747c2eee671e9e6c7d7c6e6b4b93d
dushyantsharma0 <dushyantbhaman89@gmail.com>
2024-04-19 18:01:04 +0530

    add

---
+++ routers/userRouter.js
@@ -0,0 +1,33 @@
+const express=require('express');
+const modleSchma=require('../models/user')
+const PostSchema=require('../models/postes')
+const commentsSchema=require('../models/comments')
+const LikeSchema=require('../models/likes')
+const router=express();
+
+router.get('/', function(req, res){
+ res.send('Hello World');
+})
+
+router.get('/user', async function(req, res){
+    const data = await PostSchema.find().populate([
+      {
+        path: 'author',
+        select: 'name', // Selecting the 'name' field in the 'author' path
+      },
+      {
+        path: 'comment',
+        populate:{
+          path: 'sender',
+       
+        }
+      },   { path: "like",
+      populate:{
+       path: 'users'
+      }
+   }
+    ]);
+    res.send(data);
+  });
+
+module.exports=router
