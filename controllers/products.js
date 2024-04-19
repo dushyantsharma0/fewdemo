ec6f3fbaa6aeed3b54bc06f4f30244cf1f4c74ac
dushyantsharma0 <dushyantbhaman89@gmail.com>
2024-04-19 20:48:21 +0530

    add

--- controllers/products.js
+++ controllers/products.js
@@ -1,37 +1,7 @@
 const product=require("../models/products")
 
 const getAllProducts=async(req,resp)=>{
-    const {company,name,sort,select}=req.query;
-    
-const queryObject={}
-if(company){
-    queryObject.company=company
-    
-}if(name){
-    queryObject.name={$regex: name,$options:"i"}
-}
-
-
-let apiData=product.find(queryObject)
-
-
-if(sort){
-let shortFix=sort.split(",").join(" ");
-apiData=apiData.sort(shortFix)
-}
-if(select){
-    let selectFix=select.split(",").join(" ");
-    apiData=apiData.select(selectFix)
-}
-
-// let page=Number(req.query.page) || 1;
-// let limit=Number(req.query.limit) || 3;
-// let skip=(page-1)*limit;
-
-apiData=apiData//.skip(skip).limit(limit)
-
-
-    const products=await apiData//.sort(sort)
+    let products= await product.find()
 resp.status(200).json(products)
 };
 const getAllProductsTesting=async(req,resp)=>{
